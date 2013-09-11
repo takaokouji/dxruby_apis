d89dc373a13ec5283fda8cf0a3dd299afaac572b
Kouji Takao <kouji.takao@gmail.com>
2013-09-12 00:03:13 +0900

    show progress.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def progress(val)
+    if val == 100
+      css_class = 'success'
+    elsif val == 0
+      css_class = 'warning'
+    else
+      css_class = 'info'
+    end
+    return content_tag(:div, 'class' => 'progress') {
+      content_tag(:div, '',
+                  'class' => "progress-bar progress-bar-#{css_class}",
+                  role: "progressbar",
+                  'aria-valuenow' => val.to_s,
+                  'aria-valuemin' => "0",
+                  'aria-valuemax' => "100",
+                  style: "width: #{val}%")
+    }
+  end
 end
